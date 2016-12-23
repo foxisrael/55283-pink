8f42003299bc3acc1b0d29f70ae80546c53c0020
Dmitriy Z <dmitriy.zamanskiy@gmail.com>
2016-12-23 21:33:21 +0200

    automatization

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           src: [
-            "fonts/** /*.{woff,woff2}",
+            "fonts/**/*.{woff,woff2}",
             "img/**",
             "js/**",
             "*.html"
